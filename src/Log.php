94f6dc7dda54682329f894c8f3c16349b2f14a6c
Moshe Brevda <mbrevda@gmail.com>
2014-12-16 20:44:55 +0200

    Dont forget the files!

---
+++ src/Log.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Mbrevda\Logger;
+
+use Monolog\Logger;
+
+class Log extends Logger
+{
+     /**
+     * Print a string, array or object
+     *
+     * @param mixed $data
+     */
+    public function printr()
+    {
+        $data = '';
+
+        foreach (func_get_args() as $data) {
+            $data .= print_r($data, true) . PHP_EOL;
+        }
+
+        $this->debug($data);
+    }
+
+    /**
+     * Export a string, array or object
+     *
+     * @param mixed $data
+     */
+    public function export()
+    {
+        $data = '';
+
+        foreach (func_get_args() as $data) {
+            $data .= var_export($data, true) . PHP_EOL;
+        }
+        $this->debug($data);
+    }
+
+    /**
+     * Dump a string, array or object
+     *
+     * @param mixed $data and array or object
+     */
+    public function dump()
+    {
+        ob_start();
+        foreach (func_get_args() as $data) {
+            var_dump($data);
+        }
+
+        $data = ob_get_contents();
+        ob_end_clean();
+
+        $this->debug($data);
+    }
+}
